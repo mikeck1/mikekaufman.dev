c8d867c446127a059c70c454403553b49b06ae18
Mike <mikekaufman4@gmail.com>
2020-07-23 00:48:26 +0000

    inital commit

---
+++ html/precache-manifest.5631e22fe65f13c8ebd5623a6ae0d9d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb6b687a9e72577428d87459d2de41a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59730111de47d0ae83f3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e29fc9f212d7d7319772",
+    "url": "/static/js/2.c4a43a89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4a43a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59730111de47d0ae83f3",
+    "url": "/static/js/main.18b78c3d.chunk.js"
+  },
+  {
+    "revision": "ca75aca8119ecee0968b",
+    "url": "/static/js/runtime-main.88dbd4b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ updateServer.sh
@@ -0,0 +1,2 @@
+npm run build
+scp -r ./build/* root@159.65.231.146:/var/www/mikekaufman.dev/html
